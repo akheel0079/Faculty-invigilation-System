d6c86148ca895bb4c18450799cbb7f86518d0e8d
Patan Mohammad Akheel Khan <akheelkhanp6@gmail.com>
2026-03-17 16:24:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1547,4 +1547,5 @@ def database_simple():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+     import os
+     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
